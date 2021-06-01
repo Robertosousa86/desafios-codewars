67611e62781de22d1cdbb947e480b09f9b73187d
Roberto Sousa <robertosousa186@gmail.com>
2021-06-01 03:54:16 -0300

    refatoring

---
+++ whoLikeIt/refactoring.js
@@ -0,0 +1,16 @@
+function likes(names) {
+  const isArray = Array.isArray(names)
+  if(isArray === true){
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return `${names[0]} likes this`; break;
+    case 2: return `${names[0]} and ${names[1]} like this`; break;
+    case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`; break;
+    default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+ } else {
+   console.log(`${names} is not an array`);
+ }
+}
+
+console.log(likes([]));
