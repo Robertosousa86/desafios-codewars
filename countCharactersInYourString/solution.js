25628bd42b4f1f928007a3d1b6db9f3310b67612
Roberto Sousa <robertosousa186@gmail.com>
2021-06-21 01:26:02 -0300

    tests passing: Green

--- countCharactersInYourString/solution.js
+++ countCharactersInYourString/solution.js
@@ -1,5 +1,9 @@
 function count(string) {
-  
+  const count = {};
+  string.split('').forEach((char) => {
+    count[char] = count[char] ? count[char] + 1 : 1;
+  });
+  return count;
 }
 
 module.exports = count;
--- test/count.spec.js
+++ test/count.spec.js
@@ -1,5 +1,15 @@
+const count = require('../countCharactersInYourString/solution');
+
 describe('count()', () => {
-  it('should  be  what?', () => {
-    expect(true).toBe(true);
+  it('should  be return {a: 2, b: 1} when the string is "aba" ', () => {
+    expect(count('aba')).toStrictEqual({ a: 2, b: 1 });
+  });
+
+  it('should  be return {{ r: 2, o: 2, b: 1, e: 1, t: 1 }} when the string is "roberto" ', () => {
+    expect(count('roberto')).toStrictEqual({ r: 2, o: 2, b: 1, e: 1, t: 1 });
+  });
+
+  it('should  be return {} when the string is empty ', () => {
+    expect(count('')).toStrictEqual({});
   });
 });
