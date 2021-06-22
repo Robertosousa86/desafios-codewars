ff077d2dfe3e76818732226d558ef73f82da8e18
Roberto  Sousa <robertosousa186@gmail.com>
2021-06-22 02:05:52 -0300

    creating the tests cases

--- test/getCount.spec.js
+++ test/getCount.spec.js
@@ -1,5 +1,15 @@
+const getCount = require('../vowelCount/solution');
+
 describe('getCount()', () => {
-  it('should be what?', () => {
-    expect(true).toBe(true);
+  it('should be return zero', () => {
+    expect(getCount('My pyx')).toBe(0);
+  });
+
+  it('should be return 3 when the string received are "cultura"', () => {
+    expect(getCount('cultura')).toBe(3);
+  });
+
+  it('should be return 5 when the string received are "abracadabra"', () => {
+    expect(getCount('abracadabra')).toBe(5);
   });
 });
