c5cdefde75a1b34761c22a2ad003d18f4774c5f3
Roberto Sousa <robertosousa186@gmail.com>
2021-06-25 02:33:05 -0300

    creating tests

--- test/getSum.spec.js
+++ test/getSum.spec.js
@@ -1,5 +1,15 @@
+const getSum = require('../sumOfNumbers/solution');
+
 describe('getSum()', () => {
-  it('should be return "a"our "b" when "a" and "b" are the same', () => {
-    expect(getSum()).toBe(-1);
+  it('should be return "a" our "b" when "a" and "b" are the same', () => {
+    expect(getSum(-1, -1)).toBe(-1);
+  });
+
+  it('should be return 2 when sum -1 and 2', () => {
+    expect(getSum(-1, 2)).toBe(2);
+  });
+
+  it('should be return -1 when sum -1 and 0', () => {
+    expect(getSum(-1, 0)).toBe(-1);
   });
 });
