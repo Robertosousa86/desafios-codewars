78cb4ccb49b67b20ac79447423feacfd2e029069
Roberto Sousa <robertosousa186@gmail.com>
2021-06-25 02:37:58 -0300

    solution

---
+++ sumOfNumbers/solution.js
@@ -0,0 +1,14 @@
+function getSum(a, b) {
+  if (a === b) {
+    return a || b;
+  } else {
+    let max = Math.max(a, b);
+    let min = Math.min(a, b);
+    let result = 0;
+
+    for (min; min <= max; min++) {
+      result += min;
+    }
+    return result;
+  }
+}
