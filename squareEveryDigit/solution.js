098a23b79550776e6c8e78fafb5b3e3fbb2d7efb
Roberto Sousa <robertosousa186@gmail.com>
2021-06-29 04:31:11 -0300

    solution

---
+++ squareEveryDigit/solution.js
@@ -0,0 +1,9 @@
+function squareDigits(num) {
+  const digits = num.toString().split('');
+
+  const squareDigits = digits.map((e) => e * e);
+
+  const joinNumbers = squareDigits.join('');
+
+  return parseInt(joinNumbers);
+}
