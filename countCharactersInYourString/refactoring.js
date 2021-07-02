30c464869f5b25c4b61a0d0ecdc6dfb6fb85ad66
Roberto Sousa <robertosousa186@gmail.com>
2021-07-01 22:40:35 -0300

    refactoring

--- countCharactersInYourString/refactoring.js
+++ countCharactersInYourString/refactoring.js
@@ -1,7 +1,7 @@
 function count(string) {
   const obj = {};
 
-  string.split('').filter((e) => (obj[e] = obj[e] + 1 || 1));
+  [...string].filter((e) => (obj[e] = obj[e] + 1 || 1));
 
   return obj;
 }
