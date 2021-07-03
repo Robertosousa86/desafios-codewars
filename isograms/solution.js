02c7d3ab779c4d251aec728a01335ed1d8542e06
Roberto Sousa <robertosousa186@gmail.com>
2021-07-02 22:01:45 -0300

    refactoring

--- isograms/solution.js
+++ isograms/solution.js
@@ -1,5 +1,11 @@
 function isIsogram(str) {
-  return new Set(str.toLowerCase()).size === str.length;
+  [...str.toLowerCase()].sort();
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === str[i + 1]) return false;
+  }
+
+  return true;
 }
 
 module.exports = isIsogram;
--- test/isIsogram.spec.js
+++ test/isIsogram.spec.js
@@ -1,7 +1,15 @@
 const isIsogram = require('../isograms/solution');
 
 describe('isIsogram', () => {
-  it('should be return true if string area isogram', () => {
+  it('should be return true if string are a isogram', () => {
     expect(isIsogram('Dermatoglyphics')).toBe(true);
+  });
+
+  it('should be return true if string are a isogram', () => {
+    expect(isIsogram('isogram')).toBe(true);
+  });
+
+  it('should be return false if string contains a letter case', () => {
+    expect(isIsogram('moOse')).toBe(false);
   });
 });
