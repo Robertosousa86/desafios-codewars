6802026112b9d73a861acd619f80af2d719cfb51
Juliana Lima <julianalimab88@gmail.com>
2021-07-04 21:48:42 -0300

    creating  tests

--- test/isSquare.spec.js
+++ test/isSquare.spec.js
@@ -4,4 +4,16 @@ describe('isSquare()', () => {
   it('should be return true if n is a perfect square', () => {
     expect(iSquare(4)).toBe(true);
   });
+
+  it('should be return false if n is a negative number', () => {
+    expect(iSquare(-4)).toBe(false);
+  });
+
+  it('should be return true if n is 25', () => {
+    expect(iSquare(25)).toBe(true);
+  });
+
+  it('should be return false if n is 90', () => {
+    expect(iSquare(90)).toBe(false);
+  });
 });
